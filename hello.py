c4299733704fd37ac096c90ccf1c89db1203e715
Maheshkumar <callmeelprofessor@gmail.com>
2022-05-09 23:12:12 +0530

    Initial Commit :)

---
+++ hello.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/<name>")
+def hello_world(name):
+    return escape(f"<script>alert('{name}')</script>")
+   
