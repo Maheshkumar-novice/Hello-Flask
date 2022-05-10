24592a33753b4b6771bff9fa99591065d8fef62a
Maheshkumar P <m.palanisamy@saama.com>
2022-05-10 19:01:56 +0530

    Revert hello_world

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def hello_world(name):
-    return escape(f"<script>alert('{name}')</script>")
+@app.route("/")
+def hello():
+    return "<h1>Hello Friend</h1>"
    
